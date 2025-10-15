48c3c6f4afc731a09550ac464b7fed38b6d037eb
emillyayara <ayaraemilly993@gmail.com>
2025-10-15 11:09:25 -0300

    1510

--- servidor.py
+++ servidor.py
@@ -1,7 +1,9 @@
 from flask import *
 
 app = Flask(__name__)
+app.secret_key = 'b.emilly123.5/'
 cad = ['clara,clara@gmail,99','emilly,emilly@gmail,77']
+livros = []
 log = []
 senha = "123"
 
@@ -25,6 +27,48 @@ def cadastro():
         else:
             return render_template('paginicial.html')
 
+
+@app.route('/cadlivro', methods=['POST'])
+def cadastro_livro():
+    global livros
+    nomelivro = request.form.get('nomelivro')
+    autor = request.form.get('autor')
+    livro_novo = [nomelivro, autor]
+
+    if livro_novo not in livros:
+        livros.append(livro_novo)
+        return "Livro cadastrado com sucesso!"
+    else:
+        return "Este livro já está cadastrado."
+
+
+
+
+@app.route('/removlivros', methods=['GET', 'POST'])
+def remover_livro():
+    global livros
+    mensagem = ''
+    if request.method == 'POST':
+        nomelivros = request.form.get('nome')
+        if nomelivros in livros:
+            livros.remove(nomelivros)
+            mensagem = "Livro removido com sucesso!"
+            return render_template('admin/removlivros.html', mensagem=mensagem)
+
+        else:
+            mensagem = 'Livro não encontrado!'
+    return render_template('admin/removlivros.html', mensagem=mensagem)
+
+
+@app.route('/listalivros', methods=['get'])
+def listar_livros():
+    if len(livros) > 0:
+        return render_template('admin/listalivros.html', lista=livros, livros=livros)
+    else:
+        return render_template('admin/listalivros.html',  livros=livros)
+
+
+
 @app.route('/pag_cadastro')
 def pag_cadastro():
     return render_template('cadastro.html')
@@ -32,15 +76,24 @@ def pag_cadastro():
 @app.route('/administrador', methods=['post'])
 def administrador():
     senhadig = request.form.get('senha')
+
     if senhadig == senha:
-        return render_template('cadlivros.html')
+        session['login'] = 'admin'
+        return render_template('admin/pagiframe.html')
 
     else:
-        return render_template('admin.html')
+        return render_template('admin/admin.html')
 
 @app.route('/pag_admin')
 def pag_admin():
     return render_template('admin.html')
+@app.route('/cadlivros')
+def cadlivros():
+    if session.get('login') == "admin":
+      return render_template('admin/cadlivros.html')
+    else:
+        return 'Você não tem autorização'
+
 
 @app.route('/logar', methods=['post'])
 def logar():
